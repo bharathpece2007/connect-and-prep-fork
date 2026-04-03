f7f7faf6ce53dc4ea385a0a7053d21143c1aa527
bharathkumar000 <bharathece2006@gmail.com>
2026-04-03 21:49:07 +0530

    feat: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,22 @@ import Attendance from './components/features/Attendance'
 import Library from './components/features/Library'
 import Analysis from './components/features/Analysis'
 import Notes from './components/features/Notes'
-import GroupStudy from './components/features/GroupStudy'
-import PeerTutoring from './components/features/PeerTutoring'
 import DoubtSolving from './components/features/DoubtSolving'
-import StudyMarathons from './components/features/StudyMarathons'
-import Results from './components/features/Results'
+import AcademicHub from './components/features/AcademicHub'
+import StudyZone from './components/features/StudyZone'
 import Alumni from './components/features/Alumni'
 import DashboardHome from './components/features/DashboardHome'
 import Profile from './components/features/Profile'
+import Gamification from './components/features/Gamification'
+import PlacementHub from './components/features/PlacementHub'
+import Whiteboard from './components/features/Whiteboard'
+import Timetable from './components/features/Timetable'
+import ChatForum from './components/features/ChatForum'
+import CGPACalculator from './components/features/CGPACalculator'
+import ActivityFeed from './components/features/ActivityFeed'
+import Challenges from './components/features/Challenges'
+import PersonalNotes from './components/features/PersonalNotes'
+
 import './App.css'
 
 // Protected Route Component
@@ -44,14 +52,22 @@ function App() {
                         <Route path="papers" element={<QuestionPapers />} />
                         <Route path="analysis" element={<Analysis />} />
                         <Route path="notes" element={<Notes />} />
-                        <Route path="groups" element={<GroupStudy />} />
-                        <Route path="p2p" element={<PeerTutoring />} />
-                        <Route path="marathons" element={<StudyMarathons />} />
+                        <Route path="studyzone" element={<StudyZone />} />
                         <Route path="doubts" element={<DoubtSolving />} />
-                        <Route path="results" element={<Results />} />
+                        <Route path="academic" element={<AcademicHub />} />
                         <Route path="library" element={<Library />} />
                         <Route path="attendance" element={<Attendance />} />
                         <Route path="profile" element={<Profile />} />
+                        <Route path="leaderboard" element={<Gamification />} />
+                        <Route path="placements" element={<PlacementHub />} />
+                        <Route path="whiteboard" element={<Whiteboard />} />
+                        <Route path="timetable" element={<Timetable />} />
+                        <Route path="chat" element={<ChatForum />} />
+                        <Route path="cgpa" element={<CGPACalculator />} />
+                        <Route path="feed" element={<ActivityFeed />} />
+                        <Route path="challenges" element={<Challenges />} />
+                        <Route path="my-notes" element={<PersonalNotes />} />
+
                     </Route>
                 </Routes>
             </BrowserRouter>
